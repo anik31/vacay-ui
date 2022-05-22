13808e6efb477c6a30c947e0173230e079fedd0e
Aniket Prakash <b218004@iiit-bh.ac.in>
2022-05-22 10:10:55 +0530

    remove unused code

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const hamburgerBtn = document.querySelector(".header .fa-bars");
 const hamburgerMenu = document.querySelector(".hamburger-menu");
-const closeBtn = document.querySelector(".hamburger-menu .fa-times");
 
 hamburgerBtn.addEventListener("click", ()=>{
     hamburgerMenu.classList.toggle("display-block");
